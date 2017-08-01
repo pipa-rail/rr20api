6d59528a5959298dd469049a61d565c198e3844b
Your Name <you@example.com>
2017-08-01 10:34:56 +0300

    fix user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'user/index'
-
-  get 'user/show'
 
   resources :tests
   resources :source_provider
-  resources :users
+  resources :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

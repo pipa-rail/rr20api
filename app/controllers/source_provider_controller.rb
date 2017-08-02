36f332d90dcff2a4df9f2028db7a82bb3c2a27b8
Your Name <you@example.com>
2017-08-02 18:13:10 +0300

    blahblah

--- app/controllers/source_provider_controller.rb
+++ app/controllers/source_provider_controller.rb
@@ -3,7 +3,7 @@ class SourceProviderController < ApplicationController
 
   # GET /source_providers
   def index
-    @source_providers = SourceProvider.all
+    @source_providers = SourceProvider.where(active: true)
 
     render json: @source_providers
   end
@@ -16,7 +16,17 @@ class SourceProviderController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_test
-    @source_provider = SourceProvider.find(params[:id])
+    begin
+      @source_provider = SourceProvider.where(active: true).find(params[:id])
+    rescue ActiveRecord::RecordNotFound => exception
+      record_not_found(exception)
+    end
+  end
+
+  protected
+  def record_not_found(exception)
+    render json: {error: exception.message}.to_json, status: 404
+    return
   end
 
 end

318f2a9ff7dec0bc78bf3e22ae8df0ab83ac6043
Pavel Danelian <pavel_danelian@epam.com>
2017-08-02 17:32:12 +0300

    endpoints fixed

--- app/controllers/course_result_controller.rb
+++ app/controllers/course_result_controller.rb
@@ -2,15 +2,16 @@ class CourseResultController < ApplicationController
   before_action :get_current_course, only: [:show, :history]
 
   def show
+    render json: @last_courses.last
   end
 
   def history
-    render json: @last_courses
+    render json: @last_courses.last(2)
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
   def get_current_course
-    @last_courses = CourseResult.all
+    @last_courses = CourseResult
   end
 end
